070bb92a8cc832800720c5b353dad882d41432cc
Ravi <ravisinghxxxxl@gmail.com>
2021-04-23 09:12:01 +0300

    Student class implemented

--- main.py
+++ main.py
@@ -13,16 +13,24 @@ class Library:
             print(
                 f"You have been issued {bookName}. Please keep it safe and return it within 90 days")
             self.books.remove(bookName)
+            return True
         else:
             print("Sorry! this book already has been issued to someone else. Please wait untill it has been returned")
-
+            return False
     def returnBook(self, bookName):
         self.books.append(bookName)
         print("Thanks for returning this book, Hope you enjoyed it")
 
 
 class student:
-    pass
+    
+    def requestBook(self):
+        self.book = input("Enter the name of book you want to borrow")
+        return self.book
+    
+    def returnBook(self):
+        self.book = input("Enter the name of book you want to borrow")
+        return self.book
 
 
 if __name__ == "__main__":
